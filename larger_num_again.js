a93af269524044f12f508e1f792cb9161904b869
Amber M <amberm31@protonmail.com>
2021-10-09 20:55:00 -0500

    added new files

--- larger_num_again.js
+++ larger_num_again.js
@@ -1,17 +1,19 @@
 /* https://www.w3resource.com/javascript-exercises/javascript-conditional-statements-and-loops-exercises.php
     Write a JavaScript program that accept two integers and display the larger. */
 
-num1 = parseInt(prompt('First number: ', '0'),10);
-num2 = parseInt(prompt('Second number: ', '0'),10);
+num1 = window.Number(prompt('First number: ', '0'),10);
+num2 = window.Number(prompt('Second number: ', '0'),10);
 
-function larger_num(num1, num2) {
+
+
+function larger_num(num1 = 0, num2 = 0) {
     var larger;
         if (num1 > num2) {
             larger = num1;
         } else {
             larger = num2;
         }
-    return console.log(larger)
+    return alert(`${larger} is the largest number.`)
         
     }
 
