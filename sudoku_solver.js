6215d0eea77cd6ca35e1937a7e5d51d097f73586
Amber M <amberm31@protonmail.com>
2022-03-04 15:56:06 -0600

    Add objects to allow more flexiblity in accessing box values, add js to disable html buttons and removed js if conditions that were accomplishing the same thing.

--- sudoku_solver.js
+++ sudoku_solver.js
@@ -1,4 +1,8 @@
-let boxes = []
+let allBoxes = []
+let iterationGrid = [] // a modified grid that includes new box values from generated solutions
+let boxes = {}
+let unsolvedBoxes = {}
+let solvedBoxes = {}
 let givenValuesByRows = []
 let givenValuesByColumn = []
 let givenValuesBySquare = []
@@ -20,6 +24,7 @@ let column = 1
 let squareNumber = 0
 let gridRowIndex = 0
 let gridColumnIndex = 0
+let solutionButtonClicked = false
 let solved = false
 
 const grid = [
@@ -36,37 +41,80 @@ const grid = [
     ['8', ' ', '3', '7', '2', ' ', ' ', ' ', '9'],
 ]
 
+// Takes the information from the boxes object and turns it into an array of arrays that is similar to the grid constant
+let getIterationGrid = () => {
+    iterationGrid = []
+    let column = 1
+    let thisRowArray = []
+    let thisRowIndex = 0
+    for (let box = 0; box < 81; box++) {
+        thisRowArray.push(boxes[column])
+        if (column % 9 === 0) {
+            iterationGrid.push(thisRowArray)
+            thisRowIndex++
+            thisRowArray = []
+        }
+        column++
+
+    }
+    console.log(iterationGrid)
+    return iterationGrid
+}
 
 let createGrid = () => {
     let gridContainer = document.getElementById('sudokuGrid')
-    if (boxId < 82) {
-        for (let rowIndex = 1; rowIndex < 10; rowIndex++) {
-            for (let columnIndex = 1; columnIndex < 10; columnIndex++) {
-                let gridBox = document.createElement('div')
-                let box = new Box({ boxId, boxValue, row: rowIndex, column: columnIndex, squareNumber })
-                box.squareNumber = getSquareNumber(box)
-                boxes.push(box)
-                gridBox.classList.add(`square${squareNumber}`)
-                gridBox.classList.add(`box`)
-                gridBox.innerHTML = boxValue
-                boxId++
-                column++
-                row++
-                gridContainer.appendChild(gridBox)
-            }
+    document.getElementById('createGridButton').disabled = true
+    document.getElementById('getSolutionButton').removeAttribute('disabled')
+    for (let rowIndex = 1; rowIndex < 10; rowIndex++) {
+        for (let columnIndex = 1; columnIndex < 10; columnIndex++) {
+            let gridBox = document.createElement('div')
+            let box = new Box({ boxId, boxValue, row: rowIndex, column: columnIndex, })
+            box.squareNumber = getSquareNumber(box)
+            allBoxes.push(box)
+            boxes[boxId] = box
+            gridBox.classList.add(`square${squareNumber}`)
+            gridBox.classList.add(`box`)
+            gridBox.innerHTML = boxValue
+            boxId++
+            column++
+            row++
+            gridContainer.appendChild(gridBox)
         }
-        console.log(boxes)
     }
-    getPossibleValuesByColumn()
-    getPossibleValuesByRow()
+    createSolvedAndUnSolvedBoxesObjects()
+    updateNumberOfBoxes()
+    getPossibleValuesByColumn(grid)
+    getPossibleValuesByRow(grid)
     getPossibleValuesBySquare()
     getPossibleValuesByBox()
+    getIterationGrid(grid)
+    console.log(solvedBoxes)
+    console.log(unsolvedBoxes)
+    console.log(allBoxes)
+}
+
+let createSolvedAndUnSolvedBoxesObjects = () => {
+    for (eachBox of allBoxes) {
+        if (eachBox.boxValue !== ' ') {
+            boxes[eachBox.boxId].solution = eachBox.boxValue
+            solvedBoxes[eachBox.boxId] = eachBox.boxValue
+        } else {
+            unsolvedBoxes[eachBox.boxId] = ' '
+        }
+    }
+
+}
+
+let updateNumberOfBoxes = () => {
+    numberOfEmptyBoxes = Object.keys(unsolvedBoxes).length
+    numberOfSolvedBoxes = Object.keys(solvedBoxes).length
+    console.log(`numberOfSolvedBoxes: ${numberOfSolvedBoxes}, numberOfEmptyBoxes: ${numberOfEmptyBoxes}`)
 }
 
 class Box {
-    constructor({ boxId, boxValue, row, column, squareNumber, possibleValuesRow, possibleValuesColumn, possibleValuesSquare, possibleValuesBox }) {
+    constructor({ boxId, boxValue, row, column, squareNumber, possibleValuesRow, possibleValuesColumn, possibleValuesSquare, possibleValuesBox, solution }) {
         this.boxId = boxId
-        this.boxValue = getBoxValue()
+        this.boxValue = getBoxValue(grid)
         this.row = row
         this.column = column
         this.squareNumber = squareNumber
@@ -74,13 +122,13 @@ class Box {
         this.possibleValuesColumn = possibleValuesByColumn
         this.possibleValuesSquare = possibleValuesBySquare
         this.possibleValuesBox = 1
+        this.solution = solution
     }
-
 }
 
 // Assigns proper value to each box
-let getBoxValue = () => {
-    boxValue = grid[gridRowIndex][gridColumnIndex]
+let getBoxValue = (puzzle) => {
+    boxValue = puzzle[gridRowIndex][gridColumnIndex]
     if (gridColumnIndex < 8) {
         gridColumnIndex++
     } else {
@@ -117,118 +165,106 @@ let getSquareNumber = (box) => {
 }
 
 // Generates an array of values listed in each row
-let getGivenValuesByRow = () => {
-    if (givenValuesByRows.length < 9) {
-        for (let gridRow = 0; gridRow < grid.length; gridRow++) {
-            let thisRow = grid[gridRow]
-            let valuesInThisRow = []
-            for (value of thisRow) {
-                if (value !== ' ') {
-                    valuesInThisRow.push(value)
-                }
-                valuesInThisRow.sort()
+let getGivenValuesByRow = (puzzle) => {
+    for (let gridRow = 0; gridRow < grid.length; gridRow++) {
+        let thisRow = puzzle[gridRow]
+        let valuesInThisRow = []
+        for (value of thisRow) {
+            if (value !== ' ') {
+                valuesInThisRow.push(value)
             }
-            givenValuesByRows.push(valuesInThisRow)
+            valuesInThisRow.sort()
         }
-        return givenValuesByRows
+        givenValuesByRows.push(valuesInThisRow)
     }
+    return givenValuesByRows
 }
 
 // Generates an array of values NOT listed in each row
-let getPossibleValuesByRow = () => {
-    if (possibleValuesByRows.length < 9) {
-        getGivenValuesByRow()
-        for (let row = 0; row < givenValuesByRows.length; row++) {
-            let thisRow = givenValuesByRows[row]
-            let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for (let valueIndex = 0; valueIndex < thisRow.length; valueIndex++) {
-                let eachValue = parseInt(thisRow[valueIndex])
-                let getIndex = possibleValues.indexOf(eachValue)
-                if (getIndex > -1) {
-                    possibleValues.splice(getIndex, 1)
-                }
+let getPossibleValuesByRow = (puzzle) => {
+    getGivenValuesByRow(puzzle)
+    for (let row = 0; row < givenValuesByRows.length; row++) {
+        let thisRow = givenValuesByRows[row]
+        let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for (let valueIndex = 0; valueIndex < thisRow.length; valueIndex++) {
+            let eachValue = parseInt(thisRow[valueIndex])
+            let getIndex = possibleValues.indexOf(eachValue)
+            if (getIndex > -1) {
+                possibleValues.splice(getIndex, 1)
             }
-            possibleValuesByRows.push(possibleValues)
         }
-        return possibleValuesByRows
+        possibleValuesByRows.push(possibleValues)
     }
+    return possibleValuesByRows
 }
 
 // Generates an array of values listed in each column
-let getGivenValuesByColumn = () => {
-    if (givenValuesByColumn.length < 9) {
-        let gridRow = 0
-        while (gridRow < 9) {
-            let valuesInThisColumn = []
-            for (let gridColumn = 0; gridColumn < grid.length; gridColumn++) {
-                let thisColumnValue = grid[gridColumn][gridRow]
-                if (thisColumnValue !== ' ') {
-                    valuesInThisColumn.push(thisColumnValue)
-                }
-                valuesInThisColumn.sort()
+let getGivenValuesByColumn = (puzzle) => {
+    let gridRow = 0
+    while (gridRow < 9) {
+        let valuesInThisColumn = []
+        for (let gridColumn = 0; gridColumn < grid.length; gridColumn++) {
+            let thisColumnValue = puzzle[gridColumn][gridRow]
+            if (thisColumnValue !== ' ') {
+                valuesInThisColumn.push(thisColumnValue)
             }
-            givenValuesByColumn.push(valuesInThisColumn)
-            gridRow++
+            valuesInThisColumn.sort()
         }
+        givenValuesByColumn.push(valuesInThisColumn)
+        gridRow++
     }
     return givenValuesByColumn
 }
 
 // Generates an array of values NOT listed in each column
-let getPossibleValuesByColumn = () => {
-    if (possibleValuesByColumn.length < 9) {
-        getGivenValuesByColumn()
-        for (let column = 0; column < givenValuesByColumn.length; column++) {
-            let thisColumn = givenValuesByColumn[column]
-            let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for (let valueIndex = 0; valueIndex < thisColumn.length; valueIndex++) {
-                let eachValue = parseInt(thisColumn[valueIndex])
-                let getIndex = possibleValues.indexOf(eachValue)
-                if (getIndex > -1) {
-                    possibleValues.splice(getIndex, 1)
-                }
+let getPossibleValuesByColumn = (puzzle) => {
+    getGivenValuesByColumn(puzzle)
+    for (let column = 0; column < givenValuesByColumn.length; column++) {
+        let thisColumn = givenValuesByColumn[column]
+        let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for (let valueIndex = 0; valueIndex < thisColumn.length; valueIndex++) {
+            let eachValue = parseInt(thisColumn[valueIndex])
+            let getIndex = possibleValues.indexOf(eachValue)
+            if (getIndex > -1) {
+                possibleValues.splice(getIndex, 1)
             }
-            possibleValuesByColumn.push(possibleValues)
         }
-        return possibleValuesByColumn
+        possibleValuesByColumn.push(possibleValues)
     }
+    return possibleValuesByColumn
 }
 
 // Generates an array of values listed in each square
 let getGivenValuesBySquare = () => {
-    if (givenValuesBySquare.length < 9) {
-        for (let squareIndex = 1; squareIndex < 10; squareIndex++) {
-            let valuesInThisSquare = []
-            for (eachBox of boxes) {
-                if (eachBox.squareNumber === squareIndex && eachBox.boxValue !== ' ') {
-                    valuesInThisSquare.push(eachBox.boxValue)
-                }
+    for (let squareIndex = 1; squareIndex < 10; squareIndex++) {
+        let valuesInThisSquare = []
+        for (eachBox of allBoxes) {
+            if (eachBox.squareNumber === squareIndex && eachBox.boxValue !== ' ') {
+                valuesInThisSquare.push(eachBox.boxValue)
             }
-            valuesInThisSquare.sort()
-            givenValuesBySquare.push(valuesInThisSquare)
         }
-        return givenValuesBySquare
+        valuesInThisSquare.sort()
+        givenValuesBySquare.push(valuesInThisSquare)
     }
+    return givenValuesBySquare
 }
 
 // Generates an array of values NOT listed in each square
 let getPossibleValuesBySquare = () => {
-    if (possibleValuesBySquare.length < 9) {
-        getGivenValuesBySquare()
-        for (let square = 0; square < givenValuesBySquare.length; square++) {
-            let thisSquare = givenValuesBySquare[square]
-            let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-            for (let valueIndex = 0; valueIndex < thisSquare.length; valueIndex++) {
-                let eachValue = parseInt(thisSquare[valueIndex])
-                let getIndex = possibleValues.indexOf(eachValue)
-                if (getIndex > -1) {
-                    possibleValues.splice(getIndex, 1)
-                }
+    getGivenValuesBySquare()
+    for (let square = 0; square < givenValuesBySquare.length; square++) {
+        let thisSquare = givenValuesBySquare[square]
+        let possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for (let valueIndex = 0; valueIndex < thisSquare.length; valueIndex++) {
+            let eachValue = parseInt(thisSquare[valueIndex])
+            let getIndex = possibleValues.indexOf(eachValue)
+            if (getIndex > -1) {
+                possibleValues.splice(getIndex, 1)
             }
-            possibleValuesBySquare.push(possibleValues)
         }
-        return possibleValuesBySquare
+        possibleValuesBySquare.push(possibleValues)
     }
+    return possibleValuesBySquare
 }
 
 class PossibleSolution {
@@ -242,71 +278,75 @@ class PossibleSolution {
 }
 
 let getPossibleValuesByBox = () => {
-    if (possibleSolutions.length <= numberOfEmptyBoxes) {
-        numberOfEmptyBoxes = 0
-        numberOfSolvedBoxes = 0
-        let thisBoxId = 0
-        for (eachBox of boxes) {
-            possibleValuesForThisBox = []
-            let possibleSolution;
-            if (eachBox.boxValue !== ' ') {
-                numberOfSolvedBoxes++
-            } else {
-                solution = {}
-                let possibleValuesForThisColumn = eachBox.possibleValuesColumn[eachBox.column - 1] // possible column values per box
-                let possibleValuesForThisRow = eachBox.possibleValuesRow[eachBox.row - 1] // possible row values per box
-                let possibleValuesForThisSquare = eachBox.possibleValuesSquare[eachBox.squareNumber - 1] // possible square values per box
-                for (eachColumnValue of possibleValuesForThisColumn) {
-                    for (eachRowValue of possibleValuesForThisRow) {
-                        for (eachSquareValue of possibleValuesForThisSquare) {
-                            if (eachColumnValue === eachRowValue && eachColumnValue === eachSquareValue)
-                                possibleValuesForThisBox.push(eachColumnValue) // array of possible values for this one box
-                        }
+    let thisBoxId = 0
+    for (eachBox of allBoxes) {
+        let possibleValuesForThisColumn = eachBox.possibleValuesColumn[eachBox.column - 1] // possible column values per box
+        let possibleValuesForThisRow = eachBox.possibleValuesRow[eachBox.row - 1] // possible row values per box
+        let possibleValuesForThisSquare = eachBox.possibleValuesSquare[eachBox.squareNumber - 1] // possible square values per box
+        possibleValuesForThisBox = []
+        let possibleSolution;
+        if (eachBox.boxValue === ' ') {
+            solution = {}
+            for (eachColumnValue of possibleValuesForThisColumn) {
+                for (eachRowValue of possibleValuesForThisRow) {
+                    for (eachSquareValue of possibleValuesForThisSquare) {
+                        if (eachColumnValue === eachRowValue && eachColumnValue === eachSquareValue)
+                            possibleValuesForThisBox.push(eachColumnValue) // array of possible values for this one box
                     }
                 }
-                possibleSolution = new PossibleSolution({ thisBoxId: eachBox.boxId, possibleValuesForThisColumn, possibleValuesForThisRow, possibleValuesForThisSquare, possibleValuesForThisBox })
-                possibleSolutions.push(possibleSolution)
-                thisBoxId = eachBox.boxId
-                possibleSolutionsObject[thisBoxId] = possibleSolution
-                possibleValuesByBox.push(possibleValuesForThisBox) // possible values for all boxes
-                numberOfEmptyBoxes++
             }
+            possibleSolution = new PossibleSolution({ thisBoxId: eachBox.boxId, possibleValuesForThisColumn, possibleValuesForThisRow, possibleValuesForThisSquare, possibleValuesForThisBox })
+            possibleSolutions.push(possibleSolution)
+            thisBoxId = eachBox.boxId
+            possibleSolutionsObject[thisBoxId] = possibleSolution
+            possibleValuesByBox.push(possibleValuesForThisBox) // possible values for all boxes
         }
-        console.log(`numberOfSolvedBoxes: ${numberOfSolvedBoxes}, numberOfEmptyBoxes: ${numberOfEmptyBoxes}`)
     }
+    updateNumberOfBoxes()
 }
 
 let getBoxesWithSinglePossibleSolution = () => {
-    if (boxesWithSinglePossibleSolution.length < 9) {
-        boxesWithSinglePossibleSolution = {}
-        for (eachPossibleSolution of possibleSolutions) {
-            if (eachPossibleSolution.possibleValuesForThisBox.length === 1) {
-                thisBoxId = eachPossibleSolution.thisBoxId
-                boxesWithSinglePossibleSolution[thisBoxId] = eachPossibleSolution.possibleValuesForThisBox[0]
-                console.log(`boxesWithSinglePossibleSolution: ${boxesWithSinglePossibleSolution}`)
-            }
+    boxesWithSinglePossibleSolution = {}
+    for (eachPossibleSolution of possibleSolutions) {
+        if (eachPossibleSolution.possibleValuesForThisBox.length === 1) {
+            thisBoxId = eachPossibleSolution.thisBoxId
+            boxesWithSinglePossibleSolution[thisBoxId] = eachPossibleSolution.possibleValuesForThisBox[0]
         }
-        console.log(boxesWithSinglePossibleSolution)
-        return boxesWithSinglePossibleSolution
     }
+    console.log(boxesWithSinglePossibleSolution)
+    return boxesWithSinglePossibleSolution
 }
 
 let getSolution = () => {
-    if (!solved) {
-        getBoxesWithSinglePossibleSolution()
-        let solutionContainer = document.getElementById('solutionGrid')
-        for (eachBox of boxes) {
-            let solutionBox = document.createElement('div')
-            solutionBox.classList.add(`box`)
-            if (eachBox.boxValue !== ' ') {
-                solutionBox.innerHTML = eachBox.boxValue
-            } else if (boxesWithSinglePossibleSolution.hasOwnProperty(eachBox.boxId)) {
-                let thisBoxSolution = boxesWithSinglePossibleSolution[eachBox.boxId]
-                solutionBox.innerHTML = thisBoxSolution
-                console.log(`boxId: ${eachBox.boxId}, thisBoxSolution: ${thisBoxSolution}`)
-            }
-            solutionContainer.appendChild(solutionBox)
+    document.getElementById('getSolutionButton').disabled = true
+    getBoxesWithSinglePossibleSolution()
+    let solutionContainer = document.getElementById('solutionGrid')
+    for (eachBox of allBoxes) {
+        let solutionBox = document.createElement('div')
+        solutionBox.classList.add(`box`)
+        let thisBoxId = eachBox.boxId
+        let thisBoxValue = eachBox.boxValue
+        if (thisBoxValue !== ' ') {
+            solutionBox.innerHTML = thisBoxValue
+        } else if (boxesWithSinglePossibleSolution.hasOwnProperty(thisBoxId)) {
+            let thisBoxSolution = boxesWithSinglePossibleSolution[thisBoxId]
+            solutionBox.innerHTML = thisBoxSolution
+            allBoxes[thisBoxId - 1].boxValue = `${thisBoxSolution}`
+            delete unsolvedBoxes[thisBoxId]
+            solvedBoxes[thisBoxId] = `${thisBoxSolution}`
+            boxes[thisBoxId].solution = `${thisBoxSolution}`
         }
+        solutionContainer.appendChild(solutionBox)
+    }
+    updateNumberOfBoxes()
+    console.log(unsolvedBoxes)
+    console.log(solvedBoxes)
+    solutionButtonClicked = true
+}
+
+let checkIfSudokuIsSolved = () => {
+    updateNumberOfBoxes()
+    if (Object.keys(solvedBoxes) === 81) {
         solved = true
     }
 }
