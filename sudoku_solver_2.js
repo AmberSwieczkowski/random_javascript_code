f1b6e1221ff8e9578b2f49d97434626fac1c94b0
Amber M <amberm31@protonmail.com>
2022-03-06 22:04:18 -0600

    Add SudokuGrid class and put createGrid, deleteGrid, and updateGrid functions inside class.

--- sudoku_solver_2.js
+++ sudoku_solver_2.js
@@ -1,16 +1,65 @@
-/* keys in the grid represent [square number][row number][column number][2-digit box number]
-For example: 46348 is in square 4 (out of 9), row 6 (out of 9), column 3 (out of 9), and is box number 48 (out of 81) */
+/* keys in the grid represent [row number][column number][2-digit box number][square number]
+For example: 63484 is in row 6 (out of 9), column 3 (out of 9), is box number 48 (out of 81), and is in square number 4 (out of 9) */
+
 
 const originalGrid = {
-    11101: '7', 11202: '3', 11303: ' ', 21404: '8', 21505: '6', 21606: '1', 31707: ' ', 31808: ' ', 31909: ' ',
-    12110: ' ', 12211: '9', 12312: ' ', 22413: ' ', 22514: '4', 22615: '2', 32716: '3', 32817: '7', 32918: ' ',
-    13119: '6', 13220: '4', 13321: ' ', 23422: ' ', 23523: ' ', 23624: ' ', 33725: '5', 33826: '1', 33927: '8',
-    44128: '9', 44229: ' ', 44330: '1', 54431: '4', 54532: ' ', 54633: '8', 64734: ' ', 64835: '6', 64936: ' ',
-    45137: '3', 45238: ' ', 45339: '6', 55440: ' ', 55541: ' ', 55642: '5', 65743: '4', 65844: '9', 65945: '1',
-    46146: ' ', 46247: ' ', 46348: '7', 56449: ' ', 56550: '1', 56651: '6', 66752: '8', 66853: ' ', 66954: ' ',
-    77155: ' ', 77256: '7', 77357: '4', 87458: '1', 87559: ' ', 87660: ' ', 97761: ' ', 97862: ' ', 97963: '3',
-    78164: ' ', 78265: '1', 78366: ' ', 88467: '6', 88568: '8', 88669: ' ', 98770: '7', 98871: '2', 98972: '4',
-    79173: '8', 79274: ' ', 79375: '3', 89476: '7', 89577: '2', 89678: ' ', 99779: ' ', 99880: ' ', 99981: '9'
+    11011: '7', 12021: '3', 13031: ' ', 14042: '8', 15052: '6', 16062: '1', 17073: ' ', 18083: ' ', 19093: ' ',
+    21101: ' ', 22111: '9', 23121: ' ', 24132: ' ', 25142: '4', 26152: '2', 27163: '3', 28173: '7', 29183: ' ',
+    31191: '6', 32201: '4', 33211: ' ', 34222: ' ', 35232: ' ', 36242: ' ', 37253: '5', 38263: '1', 39273: '8',
+    41284: '9', 42294: ' ', 43304: '1', 44315: '4', 45325: ' ', 46335: '8', 47346: ' ', 48356: '6', 49366: ' ',
+    51374: '3', 52384: ' ', 53394: '6', 54405: ' ', 55415: ' ', 56425: '5', 57436: '4', 58446: '9', 59456: '1',
+    61464: ' ', 62474: ' ', 63484: '7', 64495: ' ', 65505: '1', 66515: '6', 67526: '8', 68536: ' ', 69546: ' ',
+    71557: ' ', 72567: '7', 73577: '4', 74588: '1', 75598: ' ', 76608: ' ', 77619: ' ', 78629: ' ', 79639: '3',
+    81647: ' ', 82657: '1', 83667: ' ', 84678: '6', 85688: '8', 86698: ' ', 87709: '7', 88719: '2', 89729: '4',
+    91737: '8', 92747: ' ', 93757: '3', 94768: '7', 95778: '2', 96788: ' ', 97799: ' ', 98809: ' ', 99819: '9'
+}
+
+class SudokuGrid {
+    constructor(grid) {
+        this.grid = grid
+        let newBoxes = Object.entries(this.grid) // an array of arrays
+        this.allBoxes = {}
+        for (let eachBox of newBoxes) {
+            let thisBoxKey = eachBox[0]
+            let thisBoxValue = eachBox[1]
+            let thisBoxId = thisBoxKey.slice(2, 4) // Third and fourth numbers in key for box id
+            let box = new Box({ square: thisBoxKey[4], row: thisBoxKey[0], column: thisBoxKey[1], boxValue: thisBoxValue, boxId: thisBoxId })
+            this.allBoxes[thisBoxId] = box
+        }
+        console.log(this.allBoxes)
+        let [solvedBoxes, numberOfSolvedBoxes, unsolvedBoxes, numberOfUnsolvedBoxes] = getSolvedAndUnsolvedBoxes(this.allBoxes)
+        this.createGrid(this.grid)
+        // return allBoxes
+        this.updateGrid()
+    }
+
+    createGrid(grid) {
+        let gameboard = document.getElementById('sudokuGameboard')
+        let boxes = Object.entries(grid)
+        for (eachBox of boxes) {
+            let thisBoxValue = eachBox[1]
+            let sudokuBox = document.createElement('div')
+            sudokuBox.classList.add('box')
+            sudokuBox.innerHTML = thisBoxValue
+            gameboard.appendChild(sudokuBox)
+        }
+        // console.log(boxes)
+    }
+
+    deleteGrid() {
+        let gameboard = document.getElementById('sudokuGameboard')
+        while (gameboard.firstChild) {
+            gameboard.removeChild(gameboard.firstChild)
+        }
+    }
+
+    updateGrid() {
+        this.deleteGrid()
+        originalGrid[13031] = '5'
+        this.createGrid(this.grid)
+        // console.log(this.grid)
+    }
+
 }
 
 let getNewBoxes = (grid) => {
@@ -20,7 +69,7 @@ let getNewBoxes = (grid) => {
         thisBoxKey = eachBox[0]
         thisBoxValue = eachBox[1]
         thisBoxId = thisBoxKey.slice(-2) // Last two numbers from key
-        let box = new Box({square: thisBoxKey[0], row: thisBoxKey[1], column: thisBoxKey[2], boxValue: thisBoxValue, boxId: thisBoxId})
+        let box = new Box({ square: thisBoxKey[4], row: thisBoxKey[1], column: thisBoxKey[2], boxValue: thisBoxValue, boxId: thisBoxId })
         allBoxes[thisBoxId] = box
     }
     console.log(allBoxes)
@@ -64,30 +113,13 @@ class Box {
     }
 }
 
-let createGrid = (grid) => {
-    let gameboard = document.getElementById('sudokuGameboard')
-    let boxes = Object.entries(grid)
-    for (eachBox of boxes) {
-        let thisBoxValue = eachBox[1]
-        let sudokuBox = document.createElement('div')
-        sudokuBox.classList.add('box')
-        sudokuBox.innerHTML = thisBoxValue
-        gameboard.appendChild(sudokuBox)
-    }
-    // console.log(boxes)
-}
-
-let updateGame = (grid) => {
-    let allBoxes = getNewBoxes(grid)
-    // console.log(allBoxes)
-
-}
-
 let newGame = () => {
     document.getElementById('createNewGame').disabled = true
-    updateGame(originalGrid)
+    let solutionButton = document.getElementById('getSudokuSolutionButton')
+    solutionButton.removeAttribute('disabled')
+    new SudokuGrid(originalGrid)
+    // getNewBoxes(originalGrid)
+
 }
 
-newGame()
-
-
+// newGame()
